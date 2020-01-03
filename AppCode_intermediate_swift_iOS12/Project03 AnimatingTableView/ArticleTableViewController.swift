f880fa84c552e465630aa30d231d3ea6ca5dafd6
lucian <lucian@lucian.local>
2020-01-03 21:08:35 +0700

    feat: 使用CATransform和UIView.animate给table cell 设置入场动画
    tableview(_: willDisplay:forRowAt:) 设置table cell 入场动画

--- AppCode_intermediate_swift_iOS12/Project03 AnimatingTableView/ArticleTableViewController.swift
+++ AppCode_intermediate_swift_iOS12/Project03 AnimatingTableView/ArticleTableViewController.swift
@@ -6,6 +6,11 @@
 //  Copyright © 2020 oscar. All rights reserved.
 //
 
+/// 知识点
+/// 1. StoryBoard 中使用 UIStack 进行布局
+/// 2. CATransform3DMakeRotation & CATransform3DTranslate & CATransform3DIdentity & UIView.animate
+/// 3. tableview(_: willDisplay:forRowAt:) 设置table cell 入场动画
+
 import UIKit
 
 class ArticleTableViewController: UITableViewController {
@@ -20,7 +25,14 @@ class ArticleTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        tableView.estimatedRowHeight = 258.0
+        tableView.rowHeight = UITableView.automaticDimension
         
+        if #available(iOS 11.0, *) {
+            // 大的navigation title
+            navigationController?.navigationBar.prefersLargeTitles = true
+            navigationController?.navigationItem.largeTitleDisplayMode = .automatic
+        }
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -46,56 +58,27 @@ class ArticleTableViewController: UITableViewController {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath) as! ArticleTableViewCell
         cell.postTitle.text = postTitles[indexPath.row]
         cell.postImageView.image = UIImage(named: postImages[indexPath.row])
-
-        // Configure the cell...
-
         return cell
     }
-
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
+    
+    /// 在Cell绘制之前 可用于配置cell对象 和 Cell出场动画
+    override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        // 初始状态
+        cell.alpha = 0
+        // 效果1 逆时针翻转90度
+        //        let rotationAngleInRadians = 90 * CGFloat(Double.pi/180)
+//        let rotationTransform = CATransform3DMakeRotation(rotationAngleInRadians, 0, 0, 1)
+        
+        // 效果2 从左下角(x: -500, y: 100)动画到Cell本该在的地方
+        let rotationTransform = CATransform3DTranslate(CATransform3DIdentity, -500, 100, 0)
+        cell.layer.transform = rotationTransform
+       
+        
+        // 结束状态
+        UIView.animate(withDuration: 1.0) {
+            cell.alpha = 1.0
+            cell.layer.transform = CATransform3DIdentity
+        }
     }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
