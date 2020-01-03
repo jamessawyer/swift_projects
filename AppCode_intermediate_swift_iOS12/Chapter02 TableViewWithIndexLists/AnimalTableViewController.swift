b30055a7b34df06273aeb7cc9c70b1d23485feb3
lucian <lucian@lucian.local>
2020-01-03 15:58:31 +0700

    feat: Chapter02 TableView自带的sectionIndexTitle(for:)制作侧边条

---
+++ AppCode_intermediate_swift_iOS12/Chapter02 TableViewWithIndexLists/AnimalTableViewController.swift
@@ -0,0 +1,87 @@
+//
+//  AnimalTableViewController.swift
+//  Chapter02 TableViewWithIndexLists
+//
+//  Created by lucian on 2020/1/3.
+//  Copyright © 2020 oscar. All rights reserved.
+//
+
+import UIKit
+
+class AnimalTableViewController: UITableViewController {
+    let animals = ["Bear", "Black Swan", "Buffalo", "Camel", "Cockatoo", "Dog", "Donkey", "Emu", "Giraffe", "Greater Rhea", "Hippopotamus", "Horse", "Koala", "Lion", "Llama", "Manatus", "Meerkat", "Panda", "Peacock", "Pig", "Platypus", "Polar Bear", "Rhinoceros", "Seagull", "Tasmania Devil", "Whale", "Whale Shark", "Wombat"]
+    var animalsDict = [String: [String]]()
+    var animalSectionTitles = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        createAnimalDict()
+    }
+
+    // MARK: - Table view data source
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return animalSectionTitles.count
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    
+        let animalKey = animalSectionTitles[section]
+        guard let animalValues = animalsDict[animalKey] else {
+            return 0
+        }
+        return animalValues.count
+    }
+    
+    override func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        return animalSectionTitles[section]
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        let animalKey = animalSectionTitles[indexPath.section]
+        if let animalValues = animalsDict[animalKey] {
+            cell.textLabel?.text = animalValues[indexPath.row]
+            
+            let imageFilename = animalValues[indexPath.row].lowercased().replacingOccurrences(of: " ", with: "_")
+            cell.imageView?.image = UIImage(named: imageFilename)
+        }
+        return cell
+    }
+    
+    // 添加一个右边索引条
+    override func sectionIndexTitles(for tableView: UITableView) -> [String]? {
+        return animalSectionTitles
+    }
+    
+    
+
+}
+
+extension AnimalTableViewController {
+    /// 将数据转换为["B": ["Bear", "Black Swan", "Buffalo"], "C": ["Camel", "Cockatoo"]] 这种数据类型
+    func createAnimalDict() {
+        for animal in animals {
+            // 获取animal的名字
+            let firstLetterIndex = animal.index(animal.startIndex, offsetBy: 1)
+            // slice a string into a substring
+            // animal[..<firstLetterIndex] 是一个 SubString 类型
+            let animalKey = String(animal[..<firstLetterIndex])
+            
+            if var animalValues = animalsDict[animalKey] {
+                // 如果存在该key 则将其添加到值里面 并且和key联系起来
+                animalValues.append(animal)
+                animalsDict[animalKey] = animalValues
+            } else {
+                animalsDict[animalKey] = [animal]
+            }
+        }
+        
+        // 从字典的keys中获取section titles 并按升序排列 并将其转换为大写
+        animalSectionTitles = [String](animalsDict.keys).map {
+            $0.capitalized
+        }
+        animalSectionTitles = animalSectionTitles.sorted(by: { $0 < $1 })
+    }
+}
---
+++ AppCode_intermediate_swift_iOS12/Chapter02 TableViewWithIndexLists/Chapter02 TableViewWithIndexLists/AppDelegate.swift
@@ -0,0 +1,23 @@
+//
+//  AppDelegate.swift
+//  Chapter02 TableViewWithIndexLists
+//
+//  Created by lucian on 2020/1/3.
+//  Copyright © 2020 oscar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+
+}
+
