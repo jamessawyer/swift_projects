bacc16191995c9881124da39de7611135c6d7a16
lucian <lucian@lucian.local>
2020-01-03 20:42:11 +0700

    feat: 完成TableView布局

---
+++ AppCode_intermediate_swift_iOS12/Project03 AnimatingTableView/Project03 AnimatingTableView/AppDelegate.swift
@@ -0,0 +1,22 @@
+//
+//  AppDelegate.swift
+//  Project03 AnimatingTableView
+//
+//  Created by lucian on 2020/1/3.
+//  Copyright © 2020 oscar. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ AppCode_intermediate_swift_iOS12/Project03 AnimatingTableView/Project03 AnimatingTableView/ArticleTableViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  ArticleTableViewCell.swift
+//  Project03 AnimatingTableView
+//
+//  Created by lucian on 2020/1/3.
+//  Copyright © 2020 oscar. All rights reserved.
+//
+
+import UIKit
+
+class ArticleTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var postTitle: UILabel!
+    @IBOutlet weak var postImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+
+
+}
